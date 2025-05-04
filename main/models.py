abaa6797c063975d67c0b15b8f153967cb8690b7
niksnake <129598246+nikitaD03@users.noreply.github.com>
2025-05-04 19:27:57 +0500

    написал корзину,контекст процессора сделал

--- main/models.py
+++ main/models.py
@@ -1,42 +1,41 @@
 from django.db import models
-from django.urls  import reverse
+from django.urls import reverse
 
 
 class Category(models.Model):
-    name =  models.CharField(max_length=100, db_tablespace=True)
-    slug =  models.SlugField(max_length=100,  unique=True)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
 
     class Meta:
-        ordering =  ('name', )
-        verbose_name  = 'Категория'
-        verbose_name_plural =  'Категория'
-        
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
     def __str__(self):
         return self.name
     
-    def get_absolute_self(self):
-        return reverse("main:product_list_buy_category", args=[self.slug])
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
     
     
-    
+
 class Product(models.Model):
-    category =  models.ForeignKey(Category,related_name='products',
-                                  on_delete=models.CASCADE)
-
-    name  = models.CharField(max_length=100, db_index=True)
-    slug  =  models.SlugField(max_length=100, unique=100)
-    image =  models.ImageField(upload_to='products/%Y/%m/%d' , blank=True)
-    description  = models.TextField(blank=True)
-    price =  models.DecimalField(max_digits=10, decimal_places=2)
-    available =  models.BooleanField(default=True)
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=100)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    updated =  models.DateTimeField(auto_now=True)
-    
+    updated = models.DateTimeField(auto_now=True)
+
     class Meta:
-        ordering = ('name', )
-        
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
-
-    def get_absolute_irl(self):
-        return reverse("main:product_datail", args=[self.id, self.slug])    
+    
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id, self.slug])
--- main/views.py
+++ main/views.py
@@ -1,26 +1,31 @@
-from django.shortcuts import render,get_list_or_404
+
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
+
 
 
 def product_list(request, category_slug=None):
     categories = Category.objects.all()
-    products  = Product.objects.filter(available=True)
-    
+    products = Product.objects.filter(available=True)
+
     category = None
     if category_slug:
-        category = get_list_or_404(Category, slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-        
-    return render (request, 'main/product/list.html',
-                   {'category': category,
-                    'categories' :categories,
-                    'products': products})
     
-def product_detail(request, id , slug):
-    product = get_object_or_404 (Product, id=id, slug=slug, available=True)
-    related_products  = Product.objects.filter(category=product.category,
-                                               available=True).exclude(id=product.id)[:4]
-    
-    return render(request, 'main/product/detail.html',{'product': product,
-                                                       'related_products': related_products})
-            
+    return render(request, 'main/product/list.html', 
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    related_products = Product.objects.filter(category=product.category,
+                                              available=True).exclude(id=product.id)[:4]
+    cart_product_form = CartAddProductForm()
+
+    return render(request, 'main/product/detail.html', {'product': product,
+                                                        'related_products': related_products,
+                                                        'cart_product_form': cart_product_form})
